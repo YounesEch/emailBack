a04a4b92882f7a2246982fbb806a492573d99fa2
YounesEch <114940175+YounesEch@users.noreply.github.com>
2023-03-16 12:02:22 +0100

    avslut back

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+const fs = require("fs");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  fs.readFile("mails.txt", function (err, data){
+    if (err){
+      console.log(err);
+    }
+    const users= JSON.parse(data)
+    res.send(users)
+    return;  
+  })
+});
+
+
+// ska göras i en post egentligen, skriv om till en post. 
+router.post('/add', function(req, res, next) {
+  fs.readFile("mails.txt", function (err, data){
+    if (err){
+      console.log(err);
+    }
+    const users= JSON.parse(data) //hämtar filen här
+
+    let newUser = req.body;
+    newUser.id = users.length +1;
+    users.push(newUser); //vi har skapat ett nytt objekt och pushar upp den till vår JSON array.
+      //stringify menas med att skriva över den gamla
+    fs.writeFile("mails.txt", JSON.stringify(users), function(err){
+      if (err){
+        console.log(err);
+      }
+    } )  //sparar nya objektet till vår JSOn fil
+
+    res.send(users)
+    return;  
+  })
 });
 
 module.exports = router;
